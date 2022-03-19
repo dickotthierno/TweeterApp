87a2d81ec473e7df2dde6a6bd571824c619b27a0
dickotthierno <thiernomougnal@gmail.com>
2022-03-19 02:35:43 -0400

    adding a new walkthrough gif

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.kt
@@ -0,0 +1,11 @@
+package com.codepath.apps.restclienttemplate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class composeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_compose)
+    }
+}
